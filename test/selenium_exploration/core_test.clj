1518be2c98d7f3b7f20df586d213daa3aa7c41f3
Paul Williams <paul@paulswilliams.me.uk>
2017-08-10 07:13:34 +0100

    where we got to

--- test/selenium_exploration/core_test.clj
+++ test/selenium_exploration/core_test.clj
@@ -1,19 +1,18 @@
 (ns selenium-exploration.core-test
-  (:require [clojure.test :refer :all]
-            [selenium-exploration.core :refer :all])
+  (:require [clojure.test :refer :all])
   (:import (org.openqa.selenium WebDriver By)
            (org.openqa.selenium.firefox FirefoxDriver)))
 
-(def expected #{{:text "Clojure - Rationale"}
-                {:text "Clojure is boring | 8th Light"}
-                {:text "Clojure is still not for geniuses - Adam Bard and his magical blog"}
-                {:text "Clojure: the good, the bad and the ugly | No silver bullet"}
-                {:text "The Beauty of Clojure - Owen Rees-Hayward"}
-                {:text "What is Clojure NOT good for? - Google Groups"}
-                {:text "What is Clojure good at? | Hacker News"}
-                {:text "What's so great about Clojure? - Software Engineering Stack Exchange"}
-                {:text "Why Clojure's a Great Next Language for Rubyists"}
-                {:text "Why would someone learn Clojure? - Quora"}})
+
+(def expected #{{:text "What's so great about Clojure? - Software Engineering Stack Exchange"}
+                 {:text "Why would someone learn Clojure? - Quora"}
+                 {:text "The Beauty of Clojure - Owen Rees-Hayward"}
+                 {:text "Clojure is still not for geniuses - Adam Bard and his magical blog"}
+                 {:text "Clojure is boring | 8th Light"}
+                 {:text "Why Clojure's a Great Next Language for Rubyists"}
+                 {:text "What is Clojure good at? | Hacker News"}
+                 {:text "What is Clojure NOT good for? - Google Groups"}
+                 {:text "Clojure: the good, the bad and the ugly | No silver bullet"}})
 
 (defn search []
   (with-open [driver (new FirefoxDriver)]
@@ -28,9 +27,7 @@
         (fn [a] {:text (.getText a)})
         (.findElements driver (By/cssSelector "div.srg h3 a"))))))
 
-
 (deftest searching
   (testing "I can query results"
     (is
-      (= expected
-         (search)))))
+      (= (search) expected))))
